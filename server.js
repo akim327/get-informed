5d229570911a22ea06c4261ffba65c0a6eb45776
Jeff Astor <jastor@laalliance.org>
2017-06-29 17:07:43 +0000

    Billllllz

---
+++ server.js
@@ -0,0 +1,87 @@
+var express = require('express')
+var logger = require('morgan')
+var bodyParser = require('body-parser')
+var axios = require('axios')
+
+// Create an instance of the express function
+var app = express()
+
+var appState = {
+    bills: []
+}
+
+// We want our html files to be ejs instead
+app.set('view engine', 'ejs')
+
+// Serve static content
+// Everything the user will see can be found in the views folder
+app.use(express.static('views'))
+app.use(logger('dev'))
+
+// Gives the server access to user input
+// Passes info through a url and it's encoded
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.set('views', __dirname + '/views')
+
+app.get('/', function(request, response){
+    response.render('bill.ejs')
+})
+
+app.post('/bill', function(request, response){
+    var str = request.body.search
+    
+    // var searchTerm = `q=${str.replace(" ","+")}&`;
+    var endpoint =`https://api.propublica.org/congress/v1/bills/subjects/${str}.json`
+    var apiKey = 'J9COAqTYDG8ja89ZMPzFT3i9oKyCyF089Z94KU1R'    
+    
+    axios.get(endpoint, {
+        headers: {
+            'X-API-Key':  apiKey
+        }        
+    })
+    .then(res => { return res.data.results })
+    .then(json => {
+        console.log(Object.keys(json))
+        appState.bills.push(json)
+        
+        response.render('results.ejs', {bills: json})
+    })
+    .catch(err => console.log(err))
+})
+
+app.get('/congress', function(request,response){
+    response.render('congress.ejs')
+})
+
+app.post('/single-bill', function(request, response){
+    var endpoint = request.body.bill_uri
+    
+    // var searchTerm = `q=${str.replace(" ","+")}&`;
+    // var endpoint =`https://api.propublica.org/congress/v1/bills/subjects/${str}.json`
+    var apiKey = 'J9COAqTYDG8ja89ZMPzFT3i9oKyCyF089Z94KU1R'    
+    
+    axios.get(endpoint, {
+        headers: {
+            'X-API-Key':  apiKey
+        }        
+    })
+    // .then(res => { return res.data.results })
+    .then(res => {
+        var stuffIWant = res.data.results
+        console.log(stuffIWant)
+        response.render('singleBill.ejs', {data: stuffIWant})
+    })
+    .catch(err => console.log(err))    
+})
+
+// app.get('/other-route', function(req, res){
+//     response.render('other.ejs')
+// })
+
+var port = process.env.PORT || 8080
+
+app.listen(port, function(){
+    console.log('App running on port ' + port)
+})
---
+++ views/js/script.js
@@ -0,0 +1,83 @@
+function getSearchBillz(){
+    
+    var spinnerDiv = document.getElementById("spinner")
+    spinnerDiv.innerHTML =  
+     `  
+        <div class="container" id="spinner">
+        	<div class="row">
+        		<div id="loader">
+            		<div class="dot"></div>
+        			<div class="dot"></div>
+        			<div class="dot"></div>
+        			<div class="dot"></div>
+        			<div class="dot"></div>
+        			<div class="dot"></div>
+        			<div class="dot"></div>
+        			<div class="dot"></div>
+        			<div class="lading"></div>
+        		</div>
+        	</div>
+        </div>
+      `
+      
+    var str = document.getElementById("search").value;
+    // var searchTerm = `q=${str.replace(" ","+")}&`;
+    var endpoint =`https://api.propublica.org/congress/v1/bills/subjects/${str}.json`
+    var apiKey = 'J9COAqTYDG8ja89ZMPzFT3i9oKyCyF089Z94KU1R'
+    
+    // var url = endpoint + str + apiKey
+    
+    fetch(endpoint, {
+        headers: {
+            'X-API-Key':  apiKey
+        }
+    })
+    
+    .then(data => {return data.json()})
+    .then(json => {
+        
+        console.log(json)
+        
+        var finalHTML = ''
+        
+        json.results.forEach(function(item){
+           
+            
+            // var billLink
+            
+            // fetch(item.bill_uri, {
+            //     headers: {
+            //         'X-API-Key':  apiKey
+            //     }
+            // })
+            // .then(data => {return data.json()})
+            // .then(json=>{console.log(json)})
+            // .catch(err => console.log(err))
+
+            // sessionStorage.setItem('label', item.bill_uri)
+            
+            
+            finalHTML +=   `
+                <div class="card card-outline-info mb-3 text-center">
+                  
+                  <div class="card-block">
+                    <h3 class="card-title">${item.title}</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <form action="/" method="post">
+                        <button type="submit" class="btn btn-primary">Go somewhere</button>
+                    </form>
+                  </div>
+                 
+                </div>
+                <br></br>`
+        })
+        
+        var resultDiv = document.getElementById('result')
+        resultDiv.innerHTML = finalHTML
+        
+        var spinnerDiv = document.getElementById('spinner')
+        spinnerDiv.innerHTML = ''
+    })
+    
+    .catch(err => console.log(err))
+}
